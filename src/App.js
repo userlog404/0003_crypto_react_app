4765f37e9cca97d39142e5ce0091b6c68ffce23b
userlog404 <classyneon@gmail.com>
2024-03-08 19:53:08 +0000

    final functionality commit

--- src/App.js
+++ src/App.js
@@ -5,28 +5,48 @@ import { Layout, Typography, Space } from 'antd';
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className='app'>
-      <div className='navbar'>
-        <Navbar/>
-      </div>
-      <div className='main'>
-        <Layout>
-          <div className='routes'>
-            <switch>
-              <Routes exact path="/">
-                 
-              </Routes>
-            </switch>
-          </div>
-        </Layout>
-      </div>
-      <div className='footer'>
-
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
       </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router }  from 'react-router-dom';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
+
+ReactDOM.render(
   <React.StrictMode>
-      <Router>
+    <Router>
+      <Provider store={store}>
         <App />
-      </Router>
-      
-  </React.StrictMode>
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
